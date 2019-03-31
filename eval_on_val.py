f7edf1afdced213c47d3f68e64e1bb32869e8d08
YapengTian <yapengtian@rochester.edu>
2019-03-31 16:05:11 -0400

    Update eval_on_val.py

--- eval_on_val.py
+++ eval_on_val.py
@@ -51,7 +51,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_path', type=str, default='models/', help='path for saving trained models')
-    parser.add_argument('--dataset_path', type=str, default='/home/cxu-serve/p1/jchen121/dataset/A2D', help='a2d dataset')
+    parser.add_argument('--dataset_path', type=str, default='../A2D', help='a2d dataset')
     parser.add_argument('--log_step', type=int, default=10, help='step size for prining log info')
     parser.add_argument('--save_step', type=int, default=1000, help='step size for saving trained models')
     parser.add_argument('--num_cls', type=int, default=43)
