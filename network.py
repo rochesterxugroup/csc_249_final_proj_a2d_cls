03ad3c7023c3dba667a07fb0550c1872ac092c29
YapengTian <yapengtian@rochester.edu>
2019-03-31 15:43:55 -0400

    Create network.py

---
+++ network.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torch.autograd as autograd
+from torch.autograd import Variable
+import math
+
+class net(nn.Module):
+    ####
+    # define your model
+    ####
+  
