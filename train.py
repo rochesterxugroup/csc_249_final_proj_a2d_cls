eeeaa4d0d3db956fa23965020f46bd022c51cdb6
YapengTian <yapengtian@rochester.edu>
2019-03-31 16:04:23 -0400

    Update train.py

--- train.py
+++ train.py
@@ -52,13 +52,13 @@ def main(args):
             # Save the model checkpoints
             if (i + 1) % args.save_step == 0:
                 torch.save(model.state_dict(), os.path.join(
-                    args.model_path, 'MLMC_model.ckpt'))
+                    args.model_path, 'net.ckpt'))
         t2 = time.time()
         print(t2 - t1)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_path', type=str, default='models/', help='path for saving trained models')
-    parser.add_argument('--dataset_path', type=str, default='/home/cxu-serve/p1/jchen121/dataset/A2D', help='a2d dataset')
+    parser.add_argument('--dataset_path', type=str, default='../A2D', help='a2d dataset')
     parser.add_argument('--log_step', type=int, default=10, help='step size for prining log info')
     parser.add_argument('--save_step', type=int, default=1000, help='step size for saving trained models')
     parser.add_argument('--num_cls', type=int, default=43)
